7e973d0e9285e7c29371d2497edd16c760bb6990
scicluna <88948700+scicluna@users.noreply.github.com>
2023-09-30 11:37:46 -0400

    more islands!

--- components/MainStage.tsx
+++ components/MainStage.tsx
@@ -13,18 +13,10 @@ export default function MainStage({ islandChain }: MainStageProps) {
             {islandChain && islandChain.map((island, islandNumber) => (
                 <div className={`grid grid-cols-[repeat(10,1fr)] h-[${HEXSIZE}rem] max-w-fit`} key={islandNumber}>
                     {island.map(tile => (
-                        <Hex tile={tile} size={HEXSIZE} chunkSize={CHUNKDIMENSIONS} key={tile.pos} />
+                        <Hex tile={tile} size={HEXSIZE} chunkSize={CHUNKDIMENSIONS} key={`${islandNumber + '-' + tile.pos}`} />
                     ))}
                 </div>
             ))}
-            {/* {islandChain && islandChain.map((row, rowIndex) => (
-                <div className={`flex h-[${HEXSIZE}rem] min-w-fit`} key={rowIndex}
-                    style={{ marginBottom: `${-HEXSIZE * .25}rem`, marginLeft: `${rowIndex % 2 === 0 ? HEXSIZE * .503 : 0}rem` }} >
-                    {row.replace("\r", "").split("").map((char, islandIndex) => (
-                        <Hex char={char} size={HEXSIZE} key={rowIndex + islandIndex} />
-                    ))}
-                </div>
-            ))} */}
         </main>
     )
 }
